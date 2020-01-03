3690b22a30266807fd2738e300623384bc1b46cf
GIDEON NYARKO <gsnyarko@gmail.com>
2020-01-03 17:09:22 +0100

    modalpage

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
-import {ProductConsumer} from './context';
-import {Link} from 'react-router-dom';
+import { ProductConsumer } from './context';
+import { Link } from 'react-router-dom';
 
 class Modal extends Component {
     render() {
         return (
             <ProductConsumer>
                 {value => {
-                    const {modalOpen, closeModal} = value;
-                    const {img, title, price} = value.modalProduct
- 
-                    if(!modalOpen) {
-                        return null 
+                    const { modalOpen, closeModal } = value;
+                    const { img, title, price } = value.modalProduct
+
+                    if (!modalOpen) {
+                        return null
                     } else {
-                        return 
+                        return (
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <span class="close">&times;</span>
+                                    <h2>Item Added to Cart</h2>
+                                </div>
+                                <div class="modal-body">
+                                    <p> {title} </p>
+                                    <p>Some other text...</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <h3>Modal Footer</h3>
+                                </div>
+                            </div>
+
+
+
+
+                        );
                     }
                 }}
 

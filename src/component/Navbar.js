92b31718cf9671cb7684a318e35d8a7e537dd50f
GIDEON NYARKO <gsnyarko@gmail.com>
2020-01-21 22:08:40 +0100

    searchinit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -2,24 +2,48 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import car from '../logo.svg';
 import cart from '../cart.svg';
+import search from '../search.svg';
+
 import './Navbar.css'
 
 class Navbar extends Component {
+
+    // state = {
+    //     search: ''
+    // }
+
+    // updateSearch(e) {
+    //     this.setState({search: e.target.value})
+    // }
+
+
+
     render() {
         return (
             <nav className='Navbar'>
 
                 <Link to='/' className='item'>
-               <img src={car} alt='car'></img>
+                <div className='Home'>
+               <img src={car} alt='car'></img>HOME
+               </div>
                </Link>
                <Link to='/' className='item'>
-                   Products
+                   PRODUCTS
                </Link>
-               <Link to='/cart' className='click'>
-                   <div >
-                       <img src={cart} alt='cart'></img>Cart
+              
+               <Link to='/cart' className='item'>
+                   <div className='click'>
+                       <img src={cart} alt='cart'></img>CART
                    </div>
                </Link>
+
+            <Link to='/contacts' className="item">CONTACT US
+            </Link>
+            <div className='item search'>
+            <input type="text" placeholder="Search.." /> 
+            {/* onChange={()=> this.updateSearch()} */}
+          <img src={search} alt='search'></img>
+          </div>
             </nav>
         );
     }
